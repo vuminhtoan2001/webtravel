0e68d82e8aabec870ad4e7b30237ad8931a8ca91
Vu Minh Toan <vuminhtoan2001@gmail.com>
2022-03-07 07:46:08 +0700

    Upload source code

---
+++ style.js
@@ -0,0 +1,14 @@
+function show_result() {
+    alert("Bạn đã đăng ký thành công! Cho chúng tôi thông tin của bạn .");
+}
+function Email() {
+    var text1 =document.getElementById("Name");
+    var text2 =document.getElementById("Email");
+    var text3 =document.getElementById("Phone");
+    var text4 =document.getElementById("Mes");
+    text1.value="";
+    text2.value="";
+    text3.value="";
+    text4.value="";
+    alert("Bạn đã gửi thành công ! Chúng tôi sẽ liên hệ với bạn sớm nhất .");
+}
